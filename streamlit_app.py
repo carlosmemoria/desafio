7db7ce10b34f0fd00d1691ffdbbfafbde022604e
Carlos Eduardo Uchôa Brandão Memória <kdumemoria@gmail.com>
2022-08-02 12:43:05 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,13 +24,13 @@ def data_describe():
     st.write(df.describe())
 
 	col1, col2 = st.columns([3, 1]) 
-	data = np.random.randn(10, 1)
+		data = np.random.randn(10, 1)
 
-	col1.subheader("A wide column with a chart")
-	col1.line_chart(data)
+		col1.subheader("A wide column with a chart")
+		col1.line_chart(data)
 
-	col2.subheader("A narrow column with the data")
-	col2.write(data)
+		col2.subheader("A narrow column with the data")
+		col2.write(data)
 		
 def data_header():
     st.header('Cabeçalho do Dataframe')
