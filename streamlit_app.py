bfeb62bb7e3a411129e86d9174d32f82d609ab83
Carlos Eduardo Uchôa Brandão Memória <kdumemoria@gmail.com>
2022-08-02 09:06:23 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,27 @@ def home(uploaded_file):
 def data_describe():
     st.header('Estatísticas do Dataframe')
     st.write(df.describe())
+	
+	
+    count = df["target"].value_counts().values
+    anos = df["target"].value_counts().index
+    plt.figure(figsize = (24, 8))
+    plt.subplot(1, 2, 1)
+    plt.bar(anos, count, ec = "k", alpha = .6, color = "royalblue")
+    plt.xlabel("Ano")
+    plt.title("Ano de início de graduação dos alunos que realizaram o ENADE em 2016")
+    plt.subplot(1, 2, 2)
+    plt.pie(count, 
+        labels = list(anos),  
+        colors = ["#20257c", "#424ad1", "#6a8ee8", "#66bbe2", "#66dee2", "#6ce2cb", "#6ad187", "#3b7f5b"],
+        labeldistance = 1.1,
+        explode = [0, 0, 0, .1, .2, .4, .6, .8],
+        wedgeprops = {"ec": "k"}, 
+        textprops = {"fontsize": 15}, 
+        )
+    plt.axis("equal")
+    plt.title("Ano de início de graduação dos alunos que realizaram o ENADE em 2016")
+    plt.legend()
 
     n = df['target'].value_counts() [0]
     e = df['target'].value_counts() [1]
