a406848daed90f1f1a2d23d0ded9e4a02bf0645c
Carlos Eduardo Uchôa Brandão Memória <kdumemoria@gmail.com>
2022-08-02 12:44:39 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,17 +25,17 @@ def data_describe():
 
     col1, col2, col3 = st.columns(3)
 
-with col1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
+	with col1:
+	    st.header("A cat")
+	    st.image("https://static.streamlit.io/examples/cat.jpg")
 
-with col2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
+	with col2:
+	    st.header("A dog")
+	    st.image("https://static.streamlit.io/examples/dog.jpg")
 
-with col3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
+	with col3:
+	    st.header("An owl")
+	    st.image("https://static.streamlit.io/examples/owl.jpg")
 		
 def data_header():
     st.header('Cabeçalho do Dataframe')
