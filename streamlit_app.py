d35171c61cdfe9147df7100ed86ceaf8a645bf7a
Carlos Eduardo Uchôa Brandão Memória <kdumemoria@gmail.com>
2022-08-02 10:34:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ def data_describe():
     values = df["target"].value_counts().values
     labels = ['Atende','Não atende']
     
-    fig = px.histogram(df, x=values, y=[20, 14])
+    fig = px.histogram(df, x=values, nbins=20)
     st.plotly_chart(fig)
 
     # Use `hole` to create a donut-like pie chart
