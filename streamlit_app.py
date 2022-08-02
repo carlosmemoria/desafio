7071e27e848940a1340b99676d7a680bf6d63d86
Carlos Eduardo Uchôa Brandão Memória <kdumemoria@gmail.com>
2022-08-02 10:13:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,8 @@ def data_describe():
     values = df["target"].value_counts().values
     labels = ['Atende','Não atende']
     
-    fig = go.Figure([go.Bar(labels=labels, values=values)])
+    #fig = go.Figure([go.Bar(labels=labels, values=values)])
+    fig = go.Figure([go.Bar(x=labels, y=[20, 14, 23])])
     fig.show()	
 
     # Use `hole` to create a donut-like pie chart
